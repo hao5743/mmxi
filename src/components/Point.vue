a2fb1f5bb127d9059b3a9661ed67bdab6ac94d11
shaoc <hao5743@163.com>
2017-05-23 18:05:47 +0800

    add tab

--- src/components/Point.vue
+++ src/components/Point.vue
@@ -1,20 +1,38 @@
 <template>
   <div>
      <x-header :left-options="{showBack: true}">穴位详情</x-header>
-    <h2>{{ $route.query.name }}</h2>
+     <group>
+     <panel :list="list" type="1"></panel>
+     </group>
+    <!--<h2>{{ $route.query.name }}</h2>  -->
+     <tab>
+      <tab-item selected @on-item-click="onItemClick">概述</tab-item>
+      <tab-item @on-item-click="onItemClick">定位</tab-item>
+      <tab-item @on-item-click="onItemClick">取穴</tab-item>
+    </tab>
   </div>
 </template>
 
 <script>
-import { XHeader } from 'vux'
+import { XHeader,Tab,TabItem, Panel, Group } from 'vux'
 export default {
   name: 'point',
-  components:{XHeader},
+  components:{XHeader,Tab,TabItem,Panel,Group},
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      list: [{
+        src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '云门',
+        desc: '现代常用于治疗气管炎、支气管炎等。配中府、期门等主治胸肋痛。'
+      }]
     }
-  }
+  },
+   methods: {
+    onItemClick (index) {
+      console.log('on item click:', index)
+    }
+   }
 }
 </script>
 
